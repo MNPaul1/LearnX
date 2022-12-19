908287f257ff2e6b9a02247b38e8394a599a2746
MN Paul <87216532+MNPaul@users.noreply.github.com>
2022-12-19 13:58:02 -0800

    Database connectivity and cred function

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const app = express();
+const logger = require("./middleware/logger")
+const morgan = require("morgan")
+const connectDB = require("./config/db")
 
 //route files
 const bootcamps = require("./routes/bootcamps")
@@ -8,13 +10,33 @@ const bootcamps = require("./routes/bootcamps")
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
-const PORT = process.env.PORT || 3000;
+//connect to database
+connectDB()
+
+const app = express();
+
+//Body Parser
+app.use(express.json())
+
+//dev logging middleware
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan('dev'))
+}
+
 
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps)
 
 
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
   console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+//Handle unhandled rejections
+process.on("unhandledRejection",(err,promise) =>{
+  console.log(`Error: ${err.message}`)
+  //Close server and exit process
+  server.close(() => process.exit(1))
+})
