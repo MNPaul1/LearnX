12525756c76dac7945c5aa83a50f063277ad2866
MN Paul <87216532+MNPaul@users.noreply.github.com>
2023-05-09 09:49:10 -0700

    bootcamp and user relationship

--- seeder.js
+++ seeder.js
@@ -10,7 +10,7 @@ dotenv.config({path: "./config/config.env"})
 //load models
 const Bootcamp = require("./models/Bootcamps");
 const Course = require("./models/Courses");
-
+const User = require('./models/User')
 
 //connect DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,12 +21,13 @@ mongoose.connect(process.env.MONGO_URI, {
 //Read json file
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
-
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
 //Import into DB
 const importData = async () =>{
     try {
         await Bootcamp.create(bootcamps)
         await Course.create(courses)
+        await User.create(users)
         console.log("Data Imported...".green.inverse);
         process.exit()
     } catch (error) {
@@ -41,6 +42,7 @@ const deleteData = async () =>{
     try {
         await Bootcamp.deleteMany()
         await Course.deleteMany()
+        await User.deleteMany()
         console.log("Data Destroyed...".red.inverse);
         process.exit()
     } catch (error) {
