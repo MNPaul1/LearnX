54000698875694850f4a77aa774e24c308345afb
MN Paul <87216532+MNPaul@users.noreply.github.com>
2023-07-11 09:41:25 -0700

    Bootcamp Update and delete page done

--- client/src/actions/bootcamp.js
+++ client/src/actions/bootcamp.js
@@ -6,10 +6,16 @@ import {
   GET_BOOTCAMP,
   GET_BOOTCAMPS,
   BOOTCAMP_CREATED,
+  BOOTCAMP_UPDATED_ERROR,
+  BOOTCAMP_UPDATED,
+  BOOTCAMP_DELETED,
+  PHOTO_UPLOADED_ERROR,
+  PHOTO_UPLOADED,
 } from "./types";
 
 //Get current Bootcamps
 export const getBootcampById = (id) => async (dispatch) => {
+  dispatch({ type: CLEAR_BOOTCAMP });
   try {
     const res = await axios.get(`/api/v1/bootcamps/${id}`);
     dispatch({
@@ -83,14 +89,15 @@ export const createBootcamp =
     });
     try {
       const res = await axios.post("/api/v1/bootcamps", body, config);
-      console.log(res.data)
       dispatch({
         type: BOOTCAMP_CREATED,
         payload: res.data,
       });
+      dispatch(setAlert("Bootcamp Created", "success"))
+
     } catch (error) {
       let errors = error.response.data.error;
-      console.log(error)
+      console.log(error);
       if (errors) {
         errors = errors.split(",");
         errors.forEach((error) => dispatch(setAlert(error, "error")));
@@ -100,3 +107,109 @@ export const createBootcamp =
       });
     }
   };
+
+//Update Bootcamp Info
+export const updateBootcamp =
+  (
+    id,
+    name,
+    description,
+    website,
+    email,
+    careers,
+    phone,
+    housing,
+    jobAssistance,
+    jobGuarantee,
+    acceptGi
+  ) =>
+  async (dispatch) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({
+      name,
+      description,
+      website,
+      email,
+      careers,
+      phone,
+      housing,
+      jobAssistance,
+      jobGuarantee,
+      acceptGi,
+    });
+    try {
+      const res = await axios.put(`/api/v1/bootcamps/${id}`, body, config);
+      dispatch({
+        type: BOOTCAMP_UPDATED,
+        payload: res.data,
+      });
+      dispatch(setAlert("Bootcamp Updated", "success"))
+
+    } catch (error) {
+      let errors = error.response.data.error;
+      if (errors) {
+        errors = errors.split(",");
+        errors.forEach((error) => dispatch(setAlert(error, "error")));
+      }
+      dispatch({
+        type: BOOTCAMP_UPDATED_ERROR,
+      });
+    }
+  };
+
+  //Delete Bootcamp
+  export const deleteBootcamp = (id) => async (dispatch) =>{
+    try {
+      const res = await axios.delete(`/api/v1/bootcamps/${id}`)
+      dispatch({
+        type: BOOTCAMP_DELETED,
+        payload: res.data
+      })
+      dispatch(setAlert("Bootcamp Deleted", "success"))
+    } catch (error) {
+      let errors = error.response.data.error;
+      if (errors) {
+        errors = errors.split(",");
+        errors.forEach((error) => dispatch(setAlert(error, "error")));
+      }
+      dispatch({
+        type: BOOTCAMP_UPDATED_ERROR,
+      });
+    }
+  }
+
+  //Upload bootcamp Photo
+  export const uploadPhoto = (id, photo) => async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      };
+      console.log("photo ;", photo)
+      const body = JSON.parse({
+        file: photo
+      })
+      const res = await axios.put(`/api/v1/bootcamps/${id}/photo`, photo, config)
+      dispatch({
+        type: PHOTO_UPLOADED,
+        payload: res.data
+      })
+      dispatch(setAlert("Photo Uploaded", "success"))
+    } catch (error) {
+
+        let errors = error.response.data.error;
+        console.log(errors)
+        if (errors) {
+          errors = errors.split(",");
+          errors.forEach((error) => dispatch(setAlert(error, "error")));
+        }
+        dispatch({
+          type: PHOTO_UPLOADED_ERROR,
+        })
+    }
+  }
