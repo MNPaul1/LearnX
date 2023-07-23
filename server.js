b4e9af744fa204a29447a0e2477238cd37beab5b
MN Paul <87216532+MNPaul@users.noreply.github.com>
2023-07-23 01:57:24 -0700

    deploy changes

--- server.js
+++ server.js
@@ -1,62 +1,62 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger")
-const errorHandler = require("./middleware/error")
-const morgan = require("morgan")
-const connectDB = require("./config/db")
-const colors = require('colors');
-const fileupload = require('express-fileupload')
-const path = require('path')
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const rateLimit = require('express-rate-limit')
-const hpp = require('hpp')
-const cors = require('cors')
+const logger = require("./middleware/logger");
+const errorHandler = require("./middleware/error");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const fileupload = require("express-fileupload");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
 //route files
-const bootcamps = require("./routes/bootcamps")
-const courses = require("./routes/courses")
-const auth = require('./routes/auth')
-const users = require('./routes/users')
-const reviews = require('./routes/reviews')
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const reviews = require("./routes/reviews");
 //connect to database
-connectDB()
+connectDB();
 
 const app = express();
 
 //Body Parser
-app.use(express.json())
+app.use(express.json());
 
 //Cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 //dev logging middleware
-if(process.env.NODE_ENV==='development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //file uploading
 app.use(fileupload());
 
 //Sanitize data
-app.use(mongoSanitize())
+app.use(mongoSanitize());
 
 // Set security headers
-app.use(helmet())
+app.use(helmet());
 
 //Prevent XSS attacks
-app.use(xss())
+app.use(xss());
 
 //rate limiting
 const limiter = rateLimit({
-  windowMs: 10*60*1000,
-  max: 1000
-})
+  windowMs: 10 * 60 * 1000,
+  max: 1000,
+});
 
 app.use(limiter);
 
@@ -67,25 +67,33 @@ app.use(hpp());
 app.use(cors());
 
 //static folder
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //Mount routers
-app.use('/api/v1/bootcamps',bootcamps)
-app.use('/api/v1/courses',courses)
-app.use('/api/v1/auth',auth)
-app.use('/api/v1/users',users)
-app.use('/api/v1/reviews',reviews)
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
+app.use("/api/v1/reviews", reviews);
 
-app.use(errorHandler)
-
+app.use(errorHandler);
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 //Handle unhandled rejections
-process.on("unhandledRejection",(err,promise) =>{
-  console.log(`Error: ${err.message}`)
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
   //Close server and exit process
-  server.close(() => process.exit(1))
-})
+  server.close(() => process.exit(1));
+});
